3850fa4a5306970a59e4036da3e383e94515e367
Niyaz PK <niyazpk@gmail.com>
2012-09-18 23:23:20 +0530

    formatting

--- ls.js
+++ ls.js
@@ -1,42 +1,40 @@
 // localStorage wrapper
-
 ls = {
 
 	flush: localStorage.clear,
 	key: localStorage.key,
 
-	remove: function(arr){
-		/* Make sure this is an array */
-		arr = [].concat(arr); 
-		for(var i = 0; i < arr.length; i++){
+	remove: function(arr) { /* Make sure this is an array */
+		arr = [].concat(arr);
+		for (var i = 0; i < arr.length; i++) {
 			localStorage.removeItem(arr[i]);
 		}
 	},
 
-	set: function(key, value){
+	set: function(key, value) {
 		localStorage.setItem(key, JSON.stringify({
 			value: value
 		}));
 	},
 
-	get: function(key){
+	get: function(key) {
 		var str = localStorage.getItem(key);
-		
-		if(str){
+
+		if (str) {
 			return JSON.parse(str).value;
 		} else {
 			return null;
 		}
 	},
 
-	keys: function(str){
+	keys: function(str) {
 
 		var re = new RegExp(str);
 
 		var list = [];
-		for(var i = 0; i < localStorage.length; i++){
+		for (var i = 0; i < localStorage.length; i++) {
 			var key = localStorage.key(i);
-			if(!str || re.test(key)){
+			if (!str || re.test(key)) {
 				list.push(key);
 			}
 		}
